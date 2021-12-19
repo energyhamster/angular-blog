0bb40f65b37313b49284a508a801320e1495d9e9
Vlad <vla.opanasenko@gmail.com>
2021-12-19 16:28:03 +0200

    validation login forms added

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../shared/interfaces";
 
 @Component({
   selector: 'app-login-page',
@@ -14,7 +15,7 @@ export class LoginPageComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl(null, [
+      email: new FormControl('', [
         Validators.required,
         Validators.email
       ]),
@@ -26,9 +27,13 @@ export class LoginPageComponent implements OnInit {
   }
 
   submit() {
-    console.log(this.form)
     if (this.form.invalid) {
       return
+    }
+
+    const user: User = {
+       email: this.form.value.email,
+       password: this.form.value.password
     }
   }
 }
