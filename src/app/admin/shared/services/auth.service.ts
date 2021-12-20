2e69f3d07abc0cf8e823de8693a14f066551cffc
Vlad <vla.opanasenko@gmail.com>
2021-12-20 17:06:12 +0200

    Added error handling

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,37 +1,68 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {User} from "../interfaces";
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {FbAuthResponse, User} from "../interfaces";
+import {Observable, Subject, throwError} from "rxjs";
 import {environment} from "../../../../environments/environment";
-import {tap} from "rxjs/operators";
-import {FbAuthResponse} from "../../../../environments/interface";
-
+import {catchError, tap} from "rxjs/operators";
 @Injectable()
 export class AuthService {
+
+  public errors$: Subject<string> = new Subject<string>()
+
   constructor(private http: HttpClient) {}
 
-  get token(): string {
-    return ''
+  get token(): string | null {
+    const expDate = new Date(<string>localStorage.getItem('fb-token-exp'))
+    if (new Date() > expDate) {
+      this.logout()
+      return null
+    }
+    return <string>localStorage.getItem('fb-token')
   }
 
   login(user: User): Observable<any> {
+    user.returnSecureToken = true
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
-        tap(this.setToken)
+        tap(this.setToken),
+        catchError(<any>this.handleError.bind(this))
       )
   }
 
   logout() {
-
+    this.setToken(null)
   }
 
   isAuthenticated(): boolean {
     return !!this.token
   }
 
-  // @ts-ignore
-  private setToken(response) {
-    return response
+  private handleError(error: HttpErrorResponse) {
+    const {message} = error.error.error
+
+    switch (message) {
+      case 'INVALID_EMAIL':
+        this.errors$.next('Incorrect Email')
+        break
+      case 'INVALID_PASSWORD':
+        this.errors$.next('Incorrect password')
+        break
+      case 'EMAIL_NOT_FOUND':
+        this.errors$.next('Email not found')
+        break
+    }
+
+    return throwError(error)
   }
 
+  // @ts-ignore
+  private setToken(response) {
+    if (response) {
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000)
+      localStorage.setItem('fb-token', response.idToken)
+      localStorage.setItem('fb-token-exp', expDate.toString())
+    } else {
+      localStorage.clear()
+    }
+  }
 }
