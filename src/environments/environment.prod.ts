9b2929728711de9bda2296f0b0ab7586b27fc1b7
Vlad <vla.opanasenko@gmail.com>
2021-12-20 14:00:45 +0200

    Added authentication with token from Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBojj0VvLLyu9r61nP8zjMJ12tRMkvqygk'
 };
