319100ce2b9da50e106e395f6c68131094aee8b8
Vlad <vla.opanasenko@gmail.com>
2021-12-22 15:23:50 +0200

    Create page form added

--- src/app/admin/shared/interfaces.ts
+++ src/app/admin/shared/interfaces.ts
@@ -8,3 +8,11 @@ export interface FbAuthResponse {
   idToken: string
   expiresIn: string
 }
+
+export interface Post {
+  id?: string,
+  title: string,
+  content: string,
+  author: string,
+  date: Date
+}
