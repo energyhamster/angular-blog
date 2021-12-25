3cf5536db013b626a438ff7a20dafc6468f3ff5b
Vlad <vla.opanasenko@gmail.com>
2021-12-25 14:09:25 +0200

    Dashboard table added

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from "../../shared/posts.service";
+import {Post} from "../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  // @ts-ignore
+  posts: Post[] = []
+  // @ts-ignore
+  pSub: Subscription
+
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
   }
 
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
+  }
+
+  remove(id: any) {
+
+  }
 }
